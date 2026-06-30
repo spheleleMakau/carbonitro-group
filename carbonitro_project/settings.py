ea42a18930223d08d261ba3d4d4ef6f2c5415b27
spheleleMakau <missmakau9@gmail.com>
2026-06-30 18:04:35 +0200

    services layout

--- carbonitro_project/settings.py
+++ carbonitro_project/settings.py
@@ -114,6 +114,7 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STORAGES = {
     "default": {"BACKEND": "django.core.files.storage.FileSystemStorage"},
     "staticfiles": {
